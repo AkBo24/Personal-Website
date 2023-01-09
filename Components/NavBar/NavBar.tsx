f9d0f061f2bff45c7b61a0ef6f67808b0cfedfcc
Akshay Bodla <akshaybodla21@gmail.com>
2023-01-09 15:55:12 -0800

    Basic nav bar

--- Components/NavBar/NavBar.tsx
+++ Components/NavBar/NavBar.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import Link from 'next/link';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { RxHamburgerMenu } from 'react-icons/rx';
 
 const NavBar = () => {
-    return <div>NavBar</div>;
+    return (
+        <div className='absolute w-full flex p-4'>
+            <p className='mr-auto'>A</p>
+
+            <div className='md:invisible ml-auto cursor-pointer'>
+                <RxHamburgerMenu size={30} />
+            </div>
+
+            <div className='hidden md:visible md:flex gap-4'>
+                <Link href='/'>Home</Link>
+                <Link href='/'>Resume</Link>
+            </div>
+        </div>
+    );
 };
 
 export default NavBar;
