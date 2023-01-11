a77ed417e73a0774fbb636267331d4a9ae2d9eae
Akshay Bodla <akshaybodla21@gmail.com>
2023-01-11 13:06:34 -0800

    Finished basic hero section - Putting off responsiveness for later. - Removed svg stuff for now, too much work

--- Components/HeroSection/HeroSection.tsx
+++ Components/HeroSection/HeroSection.tsx
@@ -1,35 +1,40 @@
-import PictureOfMeLarge from '../PictureOfMeLarge/PictureOfMeLarge';
-import PictureOfMeSmall from '../PictureOfMeSmall/PictureOfMeSmall';
+import Portrait from '../Portrait/Portrait';
+import PortraitFrame from '../PortraitFrame/PortraitFrame';
 
 const HeroSection = () => {
     return (
         <div className='sm:pt-[4em] sm:pb-[1.25em] sm:px-[2em] lg:py-24'>
             {/* Tablet and larger */}
-            <div className='sm:hidden lg:flex flex-1 justify-center gap-40'>
-                <div className='flex flex-col gap-2 mr-9 font-sans text-slate-100'>
-                    <h1 className='heroText'>
-                        Hey :) I&apos;m <br /> Akshay Bodla.
-                    </h1>
+            <div className='sm:hidden lg:flex items-center justify-center gap-40'>
+                <div className='flex flex-col gap-6 font-sans text-slate-100'>
+                    <div>
+                        <h1 className='text-6xl mb-1'>
+                            Hey :) I am <br /> Akshay Bodla.
+                        </h1>
 
-                    <h2 className='heroTagLine'>
-                        Developer, Designer, Infinite
-                        <br />
-                        Learner
-                    </h2>
+                        <h2 className='heroTagLine'>
+                            Developer, Designer, Infinite
+                            <br />
+                            Learner
+                        </h2>
+                    </div>
 
-                    <p className='pt-10 w-[607px] text-[20px] leading-[24.2px]'>
+                    <p className='text-xl text-slate-200'>
+                        I love to build beautiful and scalabale fullstack experiences{' '}
+                        <br /> with intuitive user experience
+                    </p>
+
+                    {/* <p className='pt-10 w-[607px] text-[20px] leading-[24.2px]'>
                         I’m a senior Computer Science and Data Science at the University
                         of Wisconsin-Madison. I&apos;m interested in Full stack
                         development security, and UI/UX design.{' '}
                         <span className='font-bold'>
                             I love to learn and I especially love what I do.
                         </span>
-                    </p>
+                    </p> */}
                 </div>
 
-                <div className=''>
-                    <PictureOfMeLarge />
-                </div>
+                <Portrait imageWidth={300} imageHeight={500} />
             </div>
 
             {/* Mobile */}
@@ -42,18 +47,23 @@ const HeroSection = () => {
                     <h2 className='heroTagLine'>Developer, Designer, Infinite Learner</h2>
                 </div>
 
-                <div className='row-[3_/6]'>
-                    <PictureOfMeSmall />
+                <div className='row-[3_/_6]'>
+                    <Portrait imageWidth={200} imageHeight={400} />
                 </div>
 
-                <p className='row-[6_/_7] text-lg'>
+                <p className='text-xl text-slate-200'>
+                    I love to build beautiful and scalabale fullstack experiences <br />{' '}
+                    with intuitive user experience
+                </p>
+
+                {/* <p className='row-[6_/_7] text-lg'>
                     I’m a senior Computer Science and Data Science at the University of
                     Wisconsin-Madison. I&apos;m interested in Full stack development
                     security, and UI/UX design.{' '}
                     <span className='font-semibold'>
                         I love to learn and I especially love what I do.
                     </span>
-                </p>
+                </p> */}
             </div>
         </div>
     );
---
+++ Components/PortraitFrame/PortraitFrame.tsx
@@ -0,0 +1,180 @@
+import React from 'react';
+
+const PortraitFrame = () => {
+    return (
+        <div className='absolute bottom-0 left-0'>
+            <svg
+                width='444'
+                height='518'
+                viewBox='0 0 444 518'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'>
+                <path
+                    d='M68.4717 453.189V486.459C68.4717 487.564 69.3671 488.459 70.4717 488.459H417.5C418.605 488.459 419.5 487.564 419.5 486.459V101.27C419.5 100.166 418.605 99.2703 417.5 99.2703L347 99.2703'
+                    stroke='#EBCD52'
+                />
+                <circle cx='1' cy='1' r='1' fill='#D9D9D9' />
+                <circle cx='13' cy='1' r='1' fill='#D9D9D9' />
+                <circle cx='25' cy='1' r='1' fill='#D9D9D9' />
+                <circle cx='37' cy='1' r='1' fill='#D9D9D9' />
+                <circle cx='49' cy='1' r='1' fill='#D9D9D9' />
+                <circle cx='61' cy='1' r='1' fill='#D9D9D9' />
+                <circle cx='73' cy='1' r='1' fill='#D9D9D9' />
+                <circle cx='85' cy='1' r='1' fill='#D9D9D9' />
+                <circle cx='97' cy='1' r='1' fill='#D9D9D9' />
+                <circle cx='109' cy='1' r='1' fill='#D9D9D9' />
+                <circle cx='1' cy='13' r='1' fill='#D9D9D9' />
+                <circle cx='13' cy='13' r='1' fill='#D9D9D9' />
+                <circle cx='25' cy='13' r='1' fill='#D9D9D9' />
+                <circle cx='37' cy='13' r='1' fill='#D9D9D9' />
+                <circle cx='49' cy='13' r='1' fill='#D9D9D9' />
+                <circle cx='61' cy='13' r='1' fill='#D9D9D9' />
+                <circle cx='73' cy='13' r='1' fill='#D9D9D9' />
+                <circle cx='85' cy='13' r='1' fill='#D9D9D9' />
+                <circle cx='97' cy='13' r='1' fill='#D9D9D9' />
+                <circle cx='109' cy='13' r='1' fill='#D9D9D9' />
+                <circle cx='1' cy='25' r='1' fill='#D9D9D9' />
+                <circle cx='13' cy='25' r='1' fill='#D9D9D9' />
+                <circle cx='25' cy='25' r='1' fill='#D9D9D9' />
+                <circle cx='37' cy='25' r='1' fill='#D9D9D9' />
+                <circle cx='49' cy='25' r='1' fill='#D9D9D9' />
+                <circle cx='61' cy='25' r='1' fill='#D9D9D9' />
+                <circle cx='73' cy='25' r='1' fill='#D9D9D9' />
+                <circle cx='85' cy='25' r='1' fill='#D9D9D9' />
+                <circle cx='97' cy='25' r='1' fill='#D9D9D9' />
+                <circle cx='109' cy='25' r='1' fill='#D9D9D9' />
+                <circle cx='1' cy='37' r='1' fill='#D9D9D9' />
+                <circle cx='13' cy='37' r='1' fill='#D9D9D9' />
+                <circle cx='25' cy='37' r='1' fill='#D9D9D9' />
+                <circle cx='1' cy='49' r='1' fill='#D9D9D9' />
+                <circle cx='13' cy='49' r='1' fill='#D9D9D9' />
+                <circle cx='25' cy='49' r='1' fill='#D9D9D9' />
+                <circle cx='1' cy='61' r='1' fill='#D9D9D9' />
+                <circle cx='13' cy='61' r='1' fill='#D9D9D9' />
+                <circle cx='25' cy='61' r='1' fill='#D9D9D9' />
+                <circle cx='1' cy='73' r='1' fill='#D9D9D9' />
+                <circle cx='13' cy='73' r='1' fill='#D9D9D9' />
+                <circle cx='25' cy='73' r='1' fill='#D9D9D9' />
+                <circle cx='1' cy='85' r='1' fill='#D9D9D9' />
+                <circle cx='13' cy='85' r='1' fill='#D9D9D9' />
+                <circle cx='25' cy='85' r='1' fill='#D9D9D9' />
+                <circle cx='1' cy='97' r='1' fill='#D9D9D9' />
+                <circle cx='13' cy='97' r='1' fill='#D9D9D9' />
+                <circle cx='25' cy='97' r='1' fill='#D9D9D9' />
+                <circle cx='1' cy='109' r='1' fill='#D9D9D9' />
+                <circle cx='13' cy='109' r='1' fill='#D9D9D9' />
+                <circle cx='25' cy='109' r='1' fill='#D9D9D9' />
+                <path
+                    d='M442.849 38V50.1622H427.566V62.3243H412.283V74.4865H397'
+                    stroke='#EBCD52'
+                />
+                <path
+                    d='M55.8491 467V479.162H40.566V491.324H25.283V503.486H10'
+                    stroke='#EBCD52'
+                />
+                <circle cx='272' cy='469' r='1' fill='#D9D9D9' />
+                <circle cx='284' cy='469' r='1' fill='#D9D9D9' />
+                <circle cx='296' cy='469' r='1' fill='#D9D9D9' />
+                <circle cx='308' cy='469' r='1' fill='#D9D9D9' />
+                <circle cx='320' cy='469' r='1' fill='#D9D9D9' />
+                <circle cx='332' cy='469' r='1' fill='#D9D9D9' />
+                <circle cx='344' cy='469' r='1' fill='#D9D9D9' />
+                <circle cx='356' cy='469' r='1' fill='#D9D9D9' />
+                <circle cx='368' cy='469' r='1' fill='#D9D9D9' />
+                <circle cx='380' cy='469' r='1' fill='#D9D9D9' />
+                <circle cx='272' cy='481' r='1' fill='#D9D9D9' />
+                <circle cx='284' cy='481' r='1' fill='#D9D9D9' />
+                <circle cx='296' cy='481' r='1' fill='#D9D9D9' />
+                <circle cx='308' cy='481' r='1' fill='#D9D9D9' />
+                <circle cx='320' cy='481' r='1' fill='#D9D9D9' />
+                <circle cx='332' cy='481' r='1' fill='#D9D9D9' />
+                <circle cx='344' cy='481' r='1' fill='#D9D9D9' />
+                <circle cx='356' cy='481' r='1' fill='#D9D9D9' />
+                <circle cx='368' cy='481' r='1' fill='#D9D9D9' />
+                <circle cx='380' cy='481' r='1' fill='#D9D9D9' />
+                <circle cx='272' cy='493' r='1' fill='#D9D9D9' />
+                <circle cx='284' cy='493' r='1' fill='#D9D9D9' />
+                <circle cx='296' cy='493' r='1' fill='#D9D9D9' />
+                <circle cx='308' cy='493' r='1' fill='#D9D9D9' />
+                <circle cx='320' cy='493' r='1' fill='#D9D9D9' />
+                <circle cx='332' cy='493' r='1' fill='#D9D9D9' />
+                <circle cx='344' cy='493' r='1' fill='#D9D9D9' />
+                <circle cx='356' cy='493' r='1' fill='#D9D9D9' />
+                <circle cx='368' cy='493' r='1' fill='#D9D9D9' />
+                <circle cx='380' cy='493' r='1' fill='#D9D9D9' />
+                <circle cx='272' cy='505' r='1' fill='#D9D9D9' />
+                <circle cx='284' cy='505' r='1' fill='#D9D9D9' />
+                <circle cx='296' cy='505' r='1' fill='#D9D9D9' />
+                <circle cx='308' cy='505' r='1' fill='#D9D9D9' />
+                <circle cx='320' cy='505' r='1' fill='#D9D9D9' />
+                <circle cx='332' cy='505' r='1' fill='#D9D9D9' />
+                <circle cx='344' cy='505' r='1' fill='#D9D9D9' />
+                <circle cx='356' cy='505' r='1' fill='#D9D9D9' />
+                <circle cx='368' cy='505' r='1' fill='#D9D9D9' />
+                <circle cx='380' cy='505' r='1' fill='#D9D9D9' />
+                <circle cx='272' cy='517' r='1' fill='#D9D9D9' />
+                <circle cx='284' cy='517' r='1' fill='#D9D9D9' />
+                <circle cx='296' cy='517' r='1' fill='#D9D9D9' />
+                <circle cx='308' cy='517' r='1' fill='#D9D9D9' />
+                <circle cx='320' cy='517' r='1' fill='#D9D9D9' />
+                <circle cx='332' cy='517' r='1' fill='#D9D9D9' />
+                <circle cx='344' cy='517' r='1' fill='#D9D9D9' />
+                <circle cx='356' cy='517' r='1' fill='#D9D9D9' />
+                <circle cx='368' cy='517' r='1' fill='#D9D9D9' />
+                <circle cx='380' cy='517' r='1' fill='#D9D9D9' />
+                <circle cx='385' cy='208' r='1' fill='#D9D9D9' />
+                <circle cx='397' cy='208' r='1' fill='#D9D9D9' />
+                <circle cx='409' cy='208' r='1' fill='#D9D9D9' />
+                <circle cx='421' cy='208' r='1' fill='#D9D9D9' />
+                <circle cx='433' cy='208' r='1' fill='#D9D9D9' />
+                <circle cx='385' cy='220' r='1' fill='#D9D9D9' />
+                <circle cx='397' cy='220' r='1' fill='#D9D9D9' />
+                <circle cx='409' cy='220' r='1' fill='#D9D9D9' />
+                <circle cx='421' cy='220' r='1' fill='#D9D9D9' />
+                <circle cx='433' cy='220' r='1' fill='#D9D9D9' />
+                <circle cx='385' cy='232' r='1' fill='#D9D9D9' />
+                <circle cx='397' cy='232' r='1' fill='#D9D9D9' />
+                <circle cx='409' cy='232' r='1' fill='#D9D9D9' />
+                <circle cx='421' cy='232' r='1' fill='#D9D9D9' />
+                <circle cx='433' cy='232' r='1' fill='#D9D9D9' />
+                <circle cx='385' cy='244' r='1' fill='#D9D9D9' />
+                <circle cx='397' cy='244' r='1' fill='#D9D9D9' />
+                <circle cx='409' cy='244' r='1' fill='#D9D9D9' />
+                <circle cx='421' cy='244' r='1' fill='#D9D9D9' />
+                <circle cx='433' cy='244' r='1' fill='#D9D9D9' />
+                <circle cx='385' cy='256' r='1' fill='#D9D9D9' />
+                <circle cx='397' cy='256' r='1' fill='#D9D9D9' />
+                <circle cx='409' cy='256' r='1' fill='#D9D9D9' />
+                <circle cx='421' cy='256' r='1' fill='#D9D9D9' />
+                <circle cx='433' cy='256' r='1' fill='#D9D9D9' />
+                <circle cx='385' cy='268' r='1' fill='#D9D9D9' />
+                <circle cx='397' cy='268' r='1' fill='#D9D9D9' />
+                <circle cx='409' cy='268' r='1' fill='#D9D9D9' />
+                <circle cx='421' cy='268' r='1' fill='#D9D9D9' />
+                <circle cx='433' cy='268' r='1' fill='#D9D9D9' />
+                <circle cx='385' cy='280' r='1' fill='#D9D9D9' />
+                <circle cx='397' cy='280' r='1' fill='#D9D9D9' />
+                <circle cx='409' cy='280' r='1' fill='#D9D9D9' />
+                <circle cx='421' cy='280' r='1' fill='#D9D9D9' />
+                <circle cx='433' cy='280' r='1' fill='#D9D9D9' />
+                <circle cx='385' cy='292' r='1' fill='#D9D9D9' />
+                <circle cx='397' cy='292' r='1' fill='#D9D9D9' />
+                <circle cx='409' cy='292' r='1' fill='#D9D9D9' />
+                <circle cx='421' cy='292' r='1' fill='#D9D9D9' />
+                <circle cx='433' cy='292' r='1' fill='#D9D9D9' />
+                <circle cx='385' cy='304' r='1' fill='#D9D9D9' />
+                <circle cx='397' cy='304' r='1' fill='#D9D9D9' />
+                <circle cx='409' cy='304' r='1' fill='#D9D9D9' />
+                <circle cx='421' cy='304' r='1' fill='#D9D9D9' />
+                <circle cx='433' cy='304' r='1' fill='#D9D9D9' />
+                <circle cx='385' cy='316' r='1' fill='#D9D9D9' />
+                <circle cx='397' cy='316' r='1' fill='#D9D9D9' />
+                <circle cx='409' cy='316' r='1' fill='#D9D9D9' />
+                <circle cx='421' cy='316' r='1' fill='#D9D9D9' />
+                <circle cx='433' cy='316' r='1' fill='#D9D9D9' />
+            </svg>
+        </div>
+    );
+};
+
+export default PortraitFrame;
