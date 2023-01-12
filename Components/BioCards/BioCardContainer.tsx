cb4c910bd0d5ff309d5c7cc68ae126acf5d33f9f
Akshay Bodla <akshaybodla21@gmail.com>
2023-01-12 14:20:45 -0800

    Changed spacing, and responsive behaviors

--- Components/BioCards/BioCardContainer.tsx
+++ Components/BioCards/BioCardContainer.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import BioEngineeringCard from './BioEngineeringCard/BioEngineeringCard';
-import BioDesignerCard from './BioDesignerCard/BioDesignerCard';
+import BioDeveloperCard from './BioDeveloperCard/BioDeveloperCard';
 
 const BioCardContainer = () => {
     return (
         <div className='flex sm:flex-col lg:flex-row justify-center items-center gap-20 px-20'>
             <div className='absolute w-full h-full lg:h-[50%] bg-secondaryGold'></div>
             <BioEngineeringCard />
-            <BioDesignerCard />
+            <BioDeveloperCard />
         </div>
     );
 };
--- Components/JourneyCards/Experience Card/ExperienceCard.tsx
+++ Components/JourneyCards/Experience Card/ExperienceCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ExperienceCard = () => {
     return (
-        <div className='flex-1 p-8 drop-shadow-card bg-slate-100 rounded-md max-w-md'>
+        <div className='flex-1 p-8 drop-shadow-card bg-slate-100 rounded-md max-w-md lg:max-w-lg'>
             <div
                 className='absolute -top-5 left-[50%]
                             flex justify-center items-center 
@@ -12,7 +12,7 @@ const ExperienceCard = () => {
             </div>
 
             <div className='journeyCardTextContainer'>
-                <h3 className='journeyCardTitle text-center'>Experience</h3>
+                <h3 className='journeyCardTitle'>Recent Experience</h3>
 
                 <div>
                     <h4 className='bioCardSectionTitle'>
@@ -38,7 +38,7 @@ const ExperienceCard = () => {
                     <h4 className='bioCardSectionTitle'>
                         Engineering Intern, Last Lock{' '}
                     </h4>
-                    <p>Madison, Wi | Aug 2020 - Present</p>
+                    <p>Madison, Wi | May 2022 - Aug 2022</p>
                     <p>Lorem Ipsum</p>
                 </div>
             </div>
