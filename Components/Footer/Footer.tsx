26d11957f84478cbee1f0bb6245a7bc5be0def09
Akshay Bodla <akshaybodla21@gmail.com>
2023-01-18 23:24:13 -0800

    Added footer banner

--- Components/Footer/Footer.tsx
+++ Components/Footer/Footer.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='bg-lightGreen mt-10 p-10'>
+        <div className='bg-lightGreen mt-20 p-10 flex flex-col'>
+            <div className='z-10 self-center drop-shadow-card -mt-20 bg-[#151D3A] rounded p-5 px-20 text-center'>
+                <p className='text-slate-100 text-2xl'>
+                    Thanks for visiting. Want to reach out?
+                </p>
+            </div>
             <div className='mb-10'>
                 <h3 className='text-primaryBlue text-5xl mb-2'>My Links</h3>
                 <a
@@ -11,7 +16,7 @@ const Footer = () => {
                     Github
                 </a>
                 <a
-                    href='https://github.com/AkBo24'
+                    href='https://www.linkedin.com/in/akshay-bodla/'
                     className='block text-gray-800 text-2xl mb-2 cursor-pointer hover:underline hover:underline-offset-2'>
                     Linkedin
                 </a>
