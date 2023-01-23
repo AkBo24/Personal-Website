ff0b0246a62ed0621612a83cb1f568d882108845
Akshay Bodla <akshaybodla21@gmail.com>
2023-01-23 00:51:46 -0600

    Added animations to footer anchor links

--- Components/Footer/Footer.tsx
+++ Components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div className='bg-lightGreen mt-20 p-10 flex flex-col'>
+        <div className='bg-lightGreen mt-20 p-10 flex flex-col relative'>
             <div className='z-10 self-center drop-shadow-card -mt-20 bg-[#151D3A] rounded p-5 px-20 text-center'>
                 <p className='text-slate-100 text-2xl'>
                     Thanks for visiting. Want to reach out?
@@ -10,16 +10,22 @@ const Footer = () => {
             </div>
             <div className='mb-10 mt-4'>
                 <h3 className='text-primaryBlue text-5xl mb-2'>My Links</h3>
-                <a
-                    href='https://github.com/AkBo24'
-                    className='block text-gray-800 text-2xl mb-2 cursor-pointer hover:underline hover:underline-offset-2'>
-                    Github
-                </a>
-                <a
-                    href='https://www.linkedin.com/in/akshay-bodla/'
-                    className='block text-gray-800 text-2xl mb-2 cursor-pointer hover:underline hover:underline-offset-2'>
-                    Linkedin
-                </a>
+                {/* block text-gray-800 text-2xl mb-2 cursor-pointer hover:underline hover:underline-offset-2 */}
+                <div>
+                    <a href='https://github.com/AkBo24' className='text-gray-800'>
+                        Github
+                    </a>
+                </div>
+
+                <div>
+                    <a
+                        href='https://www.linkedin.com/in/akshay-bodla/'
+                        className='text-gray-800'
+                        //  className='block text-gray-800 text-2xl mb-2 cursor-pointer hover:underline hover:underline-offset-2'
+                    >
+                        Linkedin
+                    </a>
+                </div>
             </div>
             <div className='mb-5'>
                 <h3 className='text-primaryBlue text-5xl mb-2'>Say Hello</h3>
