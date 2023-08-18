42d07c6d146e64afd0dc3ecb185106cfa6a383aa
Akshay Bodla <akshaybodla21@gmail.com>
2023-08-18 14:51:51 -0500

    Finished basic formatting for skillset section

--- website/app/Sections/Skillset/Skillset.tsx
+++ website/app/Sections/Skillset/Skillset.tsx
@@ -3,24 +3,24 @@ import React from 'react';
 const Skillset = () => {
     return (
         <div className='items-center'>
-            <h3 className='text-4xl x'>
+            <h3 className='text-3xl x'>
                 And, I design functionally beautiful
                 <br /> applications for unique problems.
             </h3>
 
-            <div>
+            <div className='mt-14'>
                 <h3 className='text-lightPrimaryBlue text-3xl font-medium mb-4'>
                     My Skillset
                 </h3>
 
-                <div className='flex'>
+                <div className='flex gap-72'>
                     <h4 className='text-4xl'>
                         Web Development,
                         <br /> Project Management,
                         <br /> &<br /> Data science
                     </h4>
 
-                    <p>
+                    <p className='w-[294px] h-[259px]'>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                         eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
                         enim ad minim veniam, quis nostrud exercitation ullamco laboris
@@ -35,7 +35,7 @@ const Skillset = () => {
                 What I can do for you
             </h3>
 
-            <div className='flex justify-evenly'>
+            <div className='grid grid-cols-3 grid-rows-1'>
                 <div className='relative'>
                     <p className='absolute text-neutral-400 text-opacity-20 text-[64px] font-medium'>
                         01
