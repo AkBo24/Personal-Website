901afd6539baedc606d7938ddf420b40168dc2de
Akshay Bodla <akshaybodla21@gmail.com>
2023-08-19 13:18:23 -0500

    Set up footer section

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -3,14 +3,19 @@ import Landing from './Sections/Landing/Landing';
 import Skillset from './Sections/Skillset/Skillset';
 import About from './Sections/About/About';
 import Career from './Sections/Career/Career';
+import Footer from './Sections/Footer/Footer';
 
 export default function Home() {
     return (
-        <main className='flex flex-col gap-32 min-h-screen items-center justify-between p-24'>
-            <Landing />
-            <Skillset />
-            <Career />
-            <About />
-        </main>
+        <>
+            <main className='flex flex-col gap-32 min-h-screen items-center justify-between p-24'>
+                <Landing />
+                <Skillset />
+                <Career />
+                <About />
+            </main>
+
+            <Footer />
+        </>
     );
 }
