418be3bad300e9e7b1644a73b0ce1973b9dcbdeb
Akshay Bodla <akshaybodla21@gmail.com>
2023-08-19 15:28:36 -0500

    Added footer text

--- website/app/Sections/Footer/Footer.tsx
+++ website/app/Sections/Footer/Footer.tsx
@@ -2,8 +2,16 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer>
-            <h1>Footer</h1>
+        <footer className='bg-neutral-900 w-full'>
+            <h1 className='text-gold text-xl font-medium mb-4'>Thanks for visiting</h1>
+
+            <h2 className='text-lg font-medium'>Akshay Bodla —</h2>
+            <p className='mb-4'>Engineer, Product Manager</p>
+
+            <h2 className='text-lg font-medium'>Say Hello —</h2>
+            <p>akshaybodla21@gmail.com</p>
+
+            <p className='text-neutral-500'>Made by me.</p>
         </footer>
     );
 };
