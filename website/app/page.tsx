b94a367e63d134820352096bfdb43e2fd9e7ccb8
Akshay Bodla <akshaybodla21@gmail.com>
2023-08-20 11:17:58 -0500

    Made Skillset responsive
    - changed flex behavior of skillset bio
    - changed grid behavior of grid in skills section to be a 2x2 on smaller screens

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import Landing from './Sections/Landing/Landing';
 import Skillset from './Sections/Skillset/Skillset';
 import About from './Sections/About/About';
@@ -8,7 +7,7 @@ import Navbar from './Sections/Navbar/Navbar';
 
 export default function Home() {
     return (
-        <div className='bg-black'>
+        <div className=''>
             <Navbar />
             <main className='flex flex-col gap-64 min-h-screen lg:items-center p-24 text-neutral-100'>
                 <Landing />
