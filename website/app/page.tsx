ccec199c971f92d28fa79a95adf5bacc70c304e0
Akshay Bodla <akshaybodla21@gmail.com>
2023-09-09 17:49:09 -0500

    Added svg of skill behind my skillset header and removed frame

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     return (
         <div className=''>
             <Navbar />
-            <main className='flex flex-col gap-28 min-h-screen lg:items-center p-10 lg:gap-64 lg:p-24 text-neutral-100 font-inter'>
+            <main className='flex flex-col gap-28 min-h-screen lg:items-center p-10 lg:gap-64 lg:p-24 text-secondaryText font-inter'>
                 <Landing />
                 <Skillset />
                 <Career />
