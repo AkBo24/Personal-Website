f415101db825ff8236104fa3306838e42074c835
Akshay Bodla <akshaybodla21@gmail.com>
2023-09-13 16:06:12 -0500

    Added animation styling to nav bar

--- website/app/Sections/Navbar/Navbar.tsx
+++ website/app/Sections/Navbar/Navbar.tsx
@@ -5,35 +5,28 @@ const Navbar = () => {
         <>
             <div className='text-3xl mt-4 text-neutral-50 absolute left-10 border-2 border-neutral-100 w-14 h-14 rounded-sm flex justify-center items-center'>
                 <h2>A</h2>
-                <div className='-z-10 absolute w-14 h-14 bg-mint/25 -right-2 -bottom-2 rounded-sm'></div>
+                <div className='-z-10 absolute w-14 h-14 bg-mint/30 -right-2 -bottom-2 rounded-sm'></div>
             </div>
-            <nav className='fixed right-0 text-lg pt-8 pr-10 font-bold text-lightPrimaryBlue flex gap-8 z-20'>
-                <a
-                    href='#landing'
-                    className='text-gold underline underline-offset-4 decoration-gold'>
+            <nav className='hidden md:flex fixed right-0 text-lg pt-8 pr-10 font-bold text-lightPrimaryBlue gap-8 z-20'>
+                <a href='#landing' className='navLink group'>
                     Home
+                    <span className='navUnderline' />
                 </a>
-
-                <a href='#skill' className=' group hover:text-lightPrimaryBlue'>
+                <a href='#skill' className='navLink group'>
                     Skills
-                    {/* <img
-                        src='arrowleft.svg'
-                        alt='Arrow pointing left, indicating go'
-                        className='hidden group-hover:pl-1 group-hover:inline'
-                        width={20}
-                    /> */}
+                    <span className='navUnderline' />
                 </a>
-
-                <a href='#experience' className='group hover:text-lightPrimaryBlue'>
+                <a href='#experience' className='navLink group'>
                     Experience
+                    <span className='navUnderline' />
                 </a>
-
-                <a href='#projects' className='group hover:text-lightPrimaryBlue'>
+                <a href='#projects' className='navLink group'>
                     Projects
+                    <span className='navUnderline' />
                 </a>
-
-                <a href='#about' className=' group hover:text-lightPrimaryBlue'>
+                <a href='#about' className='navLink group'>
                     About
+                    <span className='navUnderline' />
                 </a>
             </nav>
         </>
