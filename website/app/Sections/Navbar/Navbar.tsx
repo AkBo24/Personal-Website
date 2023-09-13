0ab5ed33b1fd296c995a6a9542b3d8a2e9f76197
Akshay Bodla <akshaybodla21@gmail.com>
2023-09-13 17:12:38 -0500

    Added simple button for small nav

--- website/app/Sections/Navbar/Navbar.tsx
+++ website/app/Sections/Navbar/Navbar.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 
 const Navbar = () => {
+    const [openNavOverlay, setOpenNavOverlay] = useState<boolean>(false);
+
+    const openOverlay = (e: any) => {
+        setOpenNavOverlay(true);
+    };
+
     return (
         <>
             <div className='text-3xl mt-4 text-neutral-50 absolute left-10 border-2 border-neutral-100 w-14 h-14 rounded-sm flex justify-center items-center'>
                 <h2>A</h2>
                 <div className='-z-10 absolute w-14 h-14 bg-mint/30 -right-2 -bottom-2 rounded-sm'></div>
             </div>
+
+            <button
+                onClick={openOverlay}
+                className='fixed right-4 top-4 md:hidden text-lightPrimaryBlue'>
+                <div className='h-1 w-6 rounded  bg-lightPrimaryBlue mb-1' />
+                <div className='h-1 w-6 rounded  bg-lightPrimaryBlue mb-1' />
+                <div className='h-1 w-6 rounded  bg-lightPrimaryBlue mb-1' />
+            </button>
+
             <nav className='hidden md:flex fixed right-0 text-lg pt-8 pr-10 font-bold text-lightPrimaryBlue gap-8 z-20'>
                 <a href='#landing' className='navLink group'>
                     Home
